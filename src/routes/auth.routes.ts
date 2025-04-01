710b92009b3d808a4fdfc40ddc8f2b2dd655e2c3
jay rajshakha <jayrajshakha73@gmail.com>
2025-04-01 17:08:18 +0530

    add swagger

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,6 +3,73 @@ import { register, login } from "../controllers/auth.controller";
 
 const router = express.Router();
 
+// POST /register route
+/**
+ * @swagger
+ * /api/auth/signup:
+ *   post:
+ *     summary: Register a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *               password:
+ *                 type: string
+ *               name:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User successfully registered
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
+
+// POST /login route
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: User login
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 token:
+ *                   type: string
+ *       400:
+ *         description: Bad request
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
+
 router.post("/signup", register as RequestHandler);
 router.post("/login", login as RequestHandler);
 
--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import authRoutes from "./routes/auth.routes";
 import errorHandler from "./middlewares/errorHandler";
+import { setupSwagger } from "./utils/swagger";
 
 dotenv.config();
 
@@ -14,7 +15,7 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
-
+setupSwagger(app);
 app.use("/api/auth", authRoutes);
 app.use(errorHandler);
 app.listen(PORT, () => {
