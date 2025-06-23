2fd1167ef2d6aa2e15fb61d4efaf2e7de7af3959
jayrajshakha07 <jayrajshakha73@gmail.com>
2025-06-23 09:50:18 +0530

    update api

--- src/server.ts
+++ src/server.ts
@@ -4,17 +4,9 @@ import { PORT } from "./config/env";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-<<<<<<< HEAD
 import errorHandler from "./middlewares/errorHandler";
 import bodyParser from "body-parser";
 import a from "./routes/leads";
-=======
-import authRoutes from "./routes/auth.routes";
-import todoRoutes from "./routes/todo.routes";
-import errorHandler from "./middlewares/errorHandler";
-import { setupSwagger } from "./utils/swagger";
-import bodyParser from "body-parser";
->>>>>>> 197b562d7fd1aff9746ffc4d29703b6367c66f06
 
 dotenv.config();
 
@@ -28,17 +20,11 @@ app.use(
 
 app.use(helmet());
 app.use(morgan("dev"));
-<<<<<<< HEAD
 app.use(bodyParser.json());
 
 app.use("/a", a);
-=======
-setupSwagger(app);
-app.use(bodyParser.json());
 
-app.use("/auth", authRoutes);
-app.use("/todos", todoRoutes);
->>>>>>> 197b562d7fd1aff9746ffc4d29703b6367c66f06
+app.use(bodyParser.json());
 app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
